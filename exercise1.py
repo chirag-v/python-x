2bf66ee1ea19314593ccca65a5bb155c2e4fba2c
Chirag Vora <chirag740@gmail.com>
2022-08-14 01:57:01 +0530

    Create exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,14 @@
+def enterProducts():
+    buyingData = {}
+    enterDetails = True
+    while enterDetails:
+        details = input('Press A to enter itema nd Q to Quit')
+        if details == 'A':
+            product = input('Eter product name:')
+            quantity=int(input('Enter quantity:'))
+            buyingData.update({product: quantity})
+        elif details == 'Q':
+            enterDetails = False
+        else:
+            print('Please enter correct option')
+    return buyingData
